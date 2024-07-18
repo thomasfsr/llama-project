61410a1f0ac73ac6dd75ff9d5c9c6bf050856015
Thomás Freire <thomas.fsr@gmail.com>
2024-07-17 21:22:15 -0300

    fix

--- src/gpt_model.py
+++ src/gpt_model.py
@@ -3,12 +3,16 @@ from dotenv import load_dotenv
 from langchain_community.llms import Ollama
 from langchain.chains import SimpleSequentialChain
 from langchain.prompts import PromptTemplate
+from langchain_openai.chat_models import ChatOpenAI
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain.schema.document import Document
+from langchain_openai.embeddings import OpenAIEmbeddings
+from langchain_community.vectorstores.lancedb import LanceDB
 
 load_dotenv()
 key = os.getenv('openai-key')
+model = ChatOpenAI(api_key=key)
 
 def load_documents(path:str):
     doc_loader = PyPDFDirectoryLoader(path)
@@ -23,9 +27,18 @@ def split_documents(documents:list[Document]):
     )
     return text_splitter.split_documents(documents)
 
+def get_embedding_function():
+    embeddings = OpenAIEmbeddings()
+    return embeddings
+
+def add_to_lancedb(chunks:list[Document]):
+    db = LanceDB(connection='data/lancedb', embedding=get_embedding_function())
+    db.add_documents(chunks)
+
 if __name__ == '__main__':
     islp_pdf = 'data'
     documents = load_documents(islp_pdf)
+    chunks = split_documents(documents)
     print(documents[0])
 
 # def generate_text(prompt: str):
